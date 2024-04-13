175a8c78675fd527d5526a0405f92d829dcf90e6
yakumo0623 <yakumo0623@gmail.com>
2024-04-13 23:55:13 +0900

    TCP受信部分のSpeaker記述を削除

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,25 @@ WiFiServer server(4444);
 
 unsigned long action_time = millis();
 
+void log_memory_info(const char* text) {
+  // DEFAULT
+  uint32_t default_total_size = heap_caps_get_total_size(MALLOC_CAP_DEFAULT) / 1024;
+  uint32_t default_free_size = heap_caps_get_free_size(MALLOC_CAP_DEFAULT) / 1024;
+  uint32_t default_largest_free_block = heap_caps_get_largest_free_block(MALLOC_CAP_DEFAULT) / 1024;
+  // DMA
+  uint32_t dma_total_size = heap_caps_get_total_size(MALLOC_CAP_DMA) / 1024;
+  uint32_t dma_free_size = heap_caps_get_free_size(MALLOC_CAP_DMA) / 1024;
+  uint32_t dma_largest_free_block = heap_caps_get_largest_free_block(MALLOC_CAP_DMA) / 1024;
+  // SPIRAM
+  uint32_t spiram_total_size = heap_caps_get_total_size(MALLOC_CAP_SPIRAM) / 1024;
+  uint32_t spiram_free_size = heap_caps_get_free_size(MALLOC_CAP_SPIRAM) / 1024;
+  uint32_t spiram_largest_free_block = heap_caps_get_largest_free_block(MALLOC_CAP_SPIRAM) / 1024;
+
+  M5.Log.printf("%s DEFAULT: 総サイズ：%4dKB 残り：%4dKB 最大ブロック残：%3dKB\n", text, default_total_size, default_free_size, default_largest_free_block);
+  M5.Log.printf("%s DMA:     総サイズ：%4dKB 残り：%4dKB 最大ブロック残：%3dKB\n", text, dma_total_size, dma_free_size, dma_largest_free_block);
+  M5.Log.printf("%s SPIRAM:  総サイズ：%4dKB 残り：%4dKB 最大ブロック残：%3dKB\n", text, spiram_total_size, spiram_free_size, spiram_largest_free_block);
+}
+
 void setup() {
   // M5Stackの初期化
   auto cfg = M5.config();
@@ -45,7 +64,7 @@ void setup() {
   spk_cfg.pin_ws = GPIO_NUM_6;        //WS, LRCLK, LRCK
   spk_cfg.pin_data_out = GPIO_NUM_8;  //SD, SDATA
   M5.Speaker.config(spk_cfg);
-  M5.Speaker.setVolume(125);
+  M5.Speaker.setVolume(255);
   M5.Speaker.end();
 
   // アバターの初期化
@@ -67,11 +86,9 @@ void setup() {
     delay(500);
   }
 
-  // UPD開始
+  // 送受信の初期化
   udp.begin(udpPort);
   server.begin();
-
-  M5.Mic.begin();
 }
 
 const size_t buffer_count = 200;
@@ -83,6 +100,7 @@ void loop() {
   M5.update();
 
   if (M5.BtnA.wasPressed()) {
+    log_memory_info("ボタンA押下");
     action_time = millis();
     if (M5.Speaker.isRunning()) {
       M5.Speaker.end();
@@ -106,27 +124,19 @@ void loop() {
     }
   }
 
-  if (idx == 0 || idx != idx2) {
-    WiFiClient client = server.available();
-    if (client) {
-      if (M5.Mic.isRunning()) {
-        M5.Mic.end();
-        M5.Speaker.begin();
-        M5.Log.println("マイク：オフ（TCP受信）");
-        M5.Log.println("スピーカー：オン（TCP受信）");
-      }
-      while (client.connected()) {
-        int size = client.available();
-        if (size) {
-          int len = client.readBytes(buffer[idx], buffer_size);
-          idx = (idx + 1) % buffer_count;
-          // M5.Log.printf("idx: %d\n", idx);  // ★★デバッグ用★★
-          if (M5.Speaker.isRunning() && !M5.Speaker.isPlaying() && idx != idx2) {
-            M5.Speaker.playRaw((const int16_t*)buffer[idx2], buffer_size >> 1, 24000);
-            idx2 = (idx2 + 1) % buffer_count;
-            // M5.Log.printf("idx2: %d\n", idx2);  // ★★デバッグ用★★
-          }
-        }
+  WiFiClient client = server.available();
+  if (client) {
+    if (M5.Mic.isRunning()) {
+      M5.Mic.end();
+      M5.Speaker.begin();
+      M5.Log.println("マイク：オフ（TCP受信）");
+      M5.Log.println("スピーカー：オン（TCP受信）");
+    }
+    while (client.connected()) {
+      int size = client.available();
+      if (size) {
+        int len = client.readBytes(buffer[idx], buffer_size);
+        idx = (idx + 1) % buffer_count;
       }
     }
   }
@@ -134,6 +144,5 @@ void loop() {
   if (M5.Speaker.isRunning() && !M5.Speaker.isPlaying() && idx != idx2) {
     M5.Speaker.playRaw((const int16_t*)buffer[idx2], buffer_size >> 1, 24000);
     idx2 = (idx2 + 1) % buffer_count;
-    // M5.Log.printf("idx2: %d\n", idx2);  // ★★デバッグ用★★
   }
 }
